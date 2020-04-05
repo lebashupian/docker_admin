7cd6cc5eae3f24553025bebb4d4f84d474d38644
lebashupian <lebashupian163@163.com>
2020-04-05 15:26:21 +0800

    d

---
+++ admin_dr.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+source /etc/profile
+source ~/.bash_profile
+stty erase '^H'
+
+
+function exit_msg() {
+	echo "$1"
+	exit 111
+}
+
+[[ $1 == help || $1 == '' ]] && {
+	echo "help 显示帮助"
+	echo "net 查看docker网络列表,第二个参数可以跟具体的网络名称，会看到这个网络更为详细的信息"
+	echo "create 创建主机"
+	echo ""
+	exit
+}
+
+function show_ip() {
+	docker ps -a --format "{{.Names}}"|sort|while read line
+	do
+		echo ----------------$line
+		docker inspect $line|grep IPAddress|grep -o -E '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}'
+	done	
+}
+
+function show_bonding_port() {
+	docker ps -a --format "{{.Names}}"|sort|while read line
+	do
+		docker port $line
+	done
+}
+
+[[ $1 == ip ]] && {
+	show_ip
+	exit
+}
+
+[[ $1 == net ]] && {
+	docker network ls
+	[[ ! -z $2 ]] && docker network inspect $2
+	exit
+}
+[[ $1 == create || $1 == c ]] && {
+	docker ps -a --format "{{.Names}}"
+	read -p "容器名称:"  name
+	read -p "主机名称:"  host_name
+	docker network ls
+	read -p "网络名称:" net_name
+	show_ip
+	read -p "ip地址：" ipaddr
+	docker images
+	read -p "镜像名称:" image_name
+	show_bonding_port
+	read -p "端口映射:" port_mapping
+	port=''
+	for i in $port_mapping
+	do
+		port=$port" -p $i"
+	done
+	ping -c 4 -i 0.01 -w 1 $ip > /dev/null 2>&1 && exit_msg "ip是通的，可能被占用"
+	
+	str="docker create -it --name $name --hostname $host_name --net $net_name --ip $ipaddr $port $image_name /bin/bash"
+	echo $str
+	eval $str
+	exit
+}
---
+++ test.sh
@@ -0,0 +1,73 @@
+#for i `docker ps -a|grep -v NAMES|awk '{print $1}'`
+#do
+#
+#done
+
+
+image=img/centos75:new
+
+#https://www.cnblogs.com/fuyuteng/p/8847331.html
+cp -f /etc/hosts /tmp/hosts/hosts.`date +%F-%T`
+sed -i /new/d /etc/hosts
+for line in `docker ps -a --format "{{.Names}}"`
+do
+	{
+		docker stop $line -t 1  && echo "stop $line"
+		docker rm $line && echo "rm $line"			
+	} &
+done
+
+wait
+
+[[ $1 == del ]] && exit
+
+
+
+for i in {1..3}
+do
+echo ----------------------new.$i----------10.0.1.$i
+j=$(($i+2))
+docker create -it --name new.$i --hostname new_10_0_1_$i --net mynetwork1 --ip 10.0.1.$i \
+-p 192.168.137.$j:80:80 \
+-v /data/nginx:/data \
+--cpus=2 \
+--memory 100m \
+--cpuset-cpus 0,1 \
+$image /bin/bash
+sleep 1
+docker start new.$i 
+docker exec -d new.$i /usr/sbin/sshd
+echo "10.0.1.$i new.$i"	>> /etc/hosts
+done
+
+rm -f /root/.ssh/known_hosts
+#
+# docker启动的时候提示WARNING: IPv4 forwarding is disabled. Networking will not work.
+# /etc/sysctl.conf 添加net.ipv4.ip_forward=1 
+# sysctl -p
+# 重新生成docker
+
+
+for i in `cat /etc/hosts|grep new|awk '{print $2}'`
+do
+ssh-copy-id -f $i
+done
+
+for i in `cat /etc/hosts|grep new|awk '{print $2}'`
+do
+echo --------------$i
+ssh $i 'date'
+done
+
+
+#yum makecache;yum -y install epel-release;yum -y install nginx;nginx;
+exit
+for i in {3..5}
+do
+echo -------------192.168.137.$i
+nc -z -w 1 192.168.137.$i 80  && echo ok || echo failed
+done
+
+#
+#然后由 stress 命令创建四个繁忙的进程消耗 CPU 资源：
+#stress -c 4
